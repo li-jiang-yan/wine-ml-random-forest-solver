80866056294621de9026aec0d934a4b7b3c1a8c2
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:38:36 +0300

    Added the evaluation of the decision tree model as a classifier using the confusion matrix and classification report

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay, classification_report
+import matplotlib.pyplot as plt
 
 # Load dataset
 wine = datasets.load_wine()
@@ -20,3 +22,11 @@ clf.fit(X_train, y_train)
 
 # Classify test data using model
 y_pred = clf.predict(X_test)
+
+# Compute the confusion matrix
+disp = ConfusionMatrixDisplay(confusion_matrix=confusion_matrix(y_test, y_pred), display_labels=wine.target_names)
+disp.plot()
+plt.show()
+
+# Get the model classification metrics (will only show after the confusion matrix display window is closed)
+print(classification_report(y_test, y_pred, target_names=wine.target_names))
