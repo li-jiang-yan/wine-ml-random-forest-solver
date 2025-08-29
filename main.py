e28d0c9411dc401501877e0ec205e995c13885ab
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:35:55 +0300

    Added the training of the random forest model

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
 
 # Load dataset
 wine = datasets.load_wine()
@@ -12,3 +13,7 @@ X = StandardScaler().fit(X).transform(X)
 
 # Split data into training and testing data
 X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
+
+# Train model
+clf = RandomForestClassifier(random_state=1)
+clf.fit(X_train, y_train)
