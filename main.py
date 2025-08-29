9dac62ba44ea2749f52ad4bc30ca575a1ee13564
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:32:22 +0300

    Added the splitting of training and testing data

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from sklearn import datasets
 from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
 
 # Load dataset
 wine = datasets.load_wine()
@@ -8,3 +9,6 @@ y = wine.target
 
 # Scale data
 X = StandardScaler().fit(X).transform(X)
+
+# Split data into training and testing data
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
