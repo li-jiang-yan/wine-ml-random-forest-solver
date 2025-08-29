0f8a8b7bac522b9a62496098a761e7469f41bf78
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:38:06 +0300

    Added the prediction of test data using the trained classifier

--- main.py
+++ main.py
@@ -17,3 +17,6 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 # Train model
 clf = RandomForestClassifier(random_state=1)
 clf.fit(X_train, y_train)
+
+# Classify test data using model
+y_pred = clf.predict(X_test)
