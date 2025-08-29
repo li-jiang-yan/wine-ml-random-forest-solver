4fcf13aac302cfd0d0be59e20b923a94dcd6987b
li-jiang-yan <professional.jiangyan_li@outlook.com>
2025-08-29 23:31:13 +0300

    Added the scaling of feature data

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from sklearn import datasets
+from sklearn.preprocessing import StandardScaler
 
 # Load dataset
 wine = datasets.load_wine()
 X = wine.data
 y = wine.target
+
+# Scale data
+X = StandardScaler().fit(X).transform(X)
